15a70420f2bfcfc76f8e6ad72e71693c1b8c628c
judell <jon@jonudell.info>
2018-05-17 09:03:01 -0700

    wip

--- index.js
+++ index.js
@@ -17,20 +17,37 @@ function app() {
   const groupContainer = getById('groupContainer');
   createGroupInputForm(groupContainer);
 
-  const viewableParams = Object.assign({}, params);
-  delete viewableParams.username;
-  delete viewableParams.group;
   getById('viewer').innerHTML = `
+<div>  
+<p>You selected "${params.exact}". Use it to:</p>
+
+<div class="formField">
+    <input name="menu" type="radio" onchange="postAnnoForGene()">
+      post a root annotation with gene name, pmid, and doi
+    <input name="menu" type="radio" value="hpo" onchange="hpoLookup()">
+      look up an hpo term
+</div>`;
+
+  function postRootAnnoForGene() {
+    const viewableParams = Object.assign({}, params);
+    delete viewableParams.username;
+    delete viewableParams.group;
+    getById('viewer').innerHTML = `
 <div>  
 <p>Post this data as an annotation?</p>
 <pre>
 ${JSON.stringify(viewableParams, null, 2)}  
 </pre>
 </div>`;
-
-getById('postButton').innerHTML = `
+    getById('postButton').innerHTML = `
 <div id="postButton"><button onclick="postAnno()">continue</button></div>`;
+  }
+
+  function hpoLookup() {
+    getById('viewer').innerHTML = 'Coming soon.'
+  }
 }
+
 
 function postAnno() {
   const payload = createAnnotationPayload(params);
