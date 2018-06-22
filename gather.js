d2436135819c9bf8c77cfe1904eabd681187e7a6
judell <jon@jonudell.info>
2018-06-22 09:40:28 -0700

    stable, deployed

--- gather.js
+++ gather.js
@@ -77,6 +77,7 @@ function gather() {
     document.body.insertBefore(activator, document.body.firstChild)
     var opener = "width=500, height=500, toolbar=yes"
     ClinGenWindow = window.open( `https://jonudell.info/h/ClinGen/index.html`, '_clingen', opener)
+//    ClinGenWindow = window.open( `https://10.0.0.9:4443/index.html`, '_clingen', opener)
   } else if (!ClinGenWindow.closed) {    // talk to the app
     ClinGenWindow.postMessage(params, '*')
   } 
